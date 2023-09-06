2deec01dee5dedd8d84f4e596ab7e418d9f56507
usmaniraghib <javaprogrammers786@gmail.com>
2023-09-06 16:42:08 +0530

    Converted into maven project.

--- src/com/raghib/selenium/RadioClassOne.java
+++ src/com/raghib/selenium/RadioClassOne.java
@@ -1,31 +1,37 @@
 package com.raghib.selenium;
 
+import java.time.Duration;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
 
-public class RadioClassOne {
+public class RadioClassOne extends BaseClass {
+
+	public static WebDriver driver;
+	public static String browserName = "chrome";
+	public static String browserVersion = "116";
+
+	public static String url = "http://www.echoecho.com/htmlforms10.htm";
 
 	public static void main(String[] args) {
-		System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "\\Driver\\chromedriver.exe");
-		WebDriver driver = new ChromeDriver();
-		
-		driver.get("http://www.echoecho.com/htmlforms10.htm");
+		// Chrome Browser
+		driver = BaseClass.getDriver(browserName, browserVersion);
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
 		driver.manage().window().maximize();
-		
+		driver.get(url);
+
 		driver.findElement(By.xpath("//input[@value='Cheese']")).click();
 		int size = driver.findElements(By.xpath("//input[@name='group1']")).size();
-		System.out.println("Radio Option Count : "+size);
-		
-		for(int index=0; index < size; index++) {
+		System.out.println("Radio Option Count : " + size);
+
+		for (int index = 0; index < size; index++) {
 			WebElement element = driver.findElements(By.xpath("//input[@name='group1']")).get(index);
 			String el = element.toString();
 			if (el.equalsIgnoreCase("Cheese")) {
 				element.click();
 			}
 		}
-		//driver.close();
+		//BaseClass.quitDriver();
 	}
-
 }
